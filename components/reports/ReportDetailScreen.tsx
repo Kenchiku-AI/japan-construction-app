d86552f3c367790175247880f0cde352cf02c64e
therealcos <0xarowana@gmail.com>
2026-05-11 14:08:00 -1000

    fix

--- components/reports/ReportDetailScreen.tsx
+++ components/reports/ReportDetailScreen.tsx
@@ -148,7 +148,7 @@ const ReportDetailScreen: FC<ReportDetailScreenProps> = ({
         getReport();
       });
 
-      if (!isUpdateDisabled) {
+      if (isUpdateDisabled) {
         getReport();
       }
     }, [isUpdateDisabled]),
