3cc11b336c6e79c60e9fdfb4fcd3a633bcaee87d
therealcos <0xarowana@gmail.com>
2026-05-12 11:59:28 -1000

    fix language

--- context/settings/useSettingsData.ts
+++ context/settings/useSettingsData.ts
@@ -1,7 +1,7 @@
 import { useState } from 'react';
 
 export const useSettingsData = () => {
-  const [reportOutputLanguage, setReportOutputLanguage] = useState('English');
+  const [reportOutputLanguage, setReportOutputLanguage] = useState('日本語');
 
   return {
     reportOutputLanguage,
