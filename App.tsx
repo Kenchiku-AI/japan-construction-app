392d88e18f1b1a18e1a8dec90845223882afa1cb
therealcos <0xarowana@gmail.com>
2026-05-22 09:35:11 -1000

    fix

--- App.tsx
+++ App.tsx
@@ -14,7 +14,6 @@ import { Loader } from './components/shared/Loader';
 import { accessTokenStorageKey, refreshTokenStorageKey } from './constants';
 import { useApi } from './services/api/useApi';
 import { GestureHandlerRootView } from 'react-native-gesture-handler';
-import Orientation from 'react-native-orientation-locker';
 
 const App = () => {
   return (
@@ -38,8 +37,6 @@ const Root = () => {
   const api = useApi();
 
   useEffect(() => {
-    Orientation.lockToPortrait();
-
     (async () => {
       const refreshCreds = await Keychain.getGenericPassword({
         service: refreshTokenStorageKey,
@@ -64,10 +61,6 @@ const Root = () => {
 
       setIsLoading(false);
     })();
-
-    return () => {
-      Orientation.unlockAllOrientations();
-    };
   }, []);
 
   if (isLoading) {
